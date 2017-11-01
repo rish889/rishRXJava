39876ff46c8a05a0ebb535e6de86a9ad509886fe
rish889 <rish889@gmail.com>
2017-11-01 23:39:48 +0530

    Map

--- src/test/java/RXJava1.java
+++ src/test/java/RXJava1.java
@@ -1,18 +1,29 @@
 import io.reactivex.Observable;
+
 import static junit.framework.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class RXJava1 {
 
-    String result="";
+    String result = "";
 
     // Simple subscription to a fix value
     @Test
-    public void returnAValue(){
+    public void returnAValue() {
         result = "";
         Observable<String> observer = Observable.just("Hello"); // provides datea
-        observer.subscribe(s -> result=s); // Callable as subscriber
+        observer.subscribe(s -> result = s); // Callable as subscriber
         assertTrue(result.equals("Hello"));
+    }
+
+    @Test
+    public void mapTest() {
+        Observable.range(0, 5)
+                .doOnNext(System.out::println)
+                .map((Integer i) -> i + 10)
+                .subscribe(System.out::println);
+
     }
 
 }
